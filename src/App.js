a73c0a42aabcc8a7aa46cf29d1d901b6e519d243
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2020-08-13 00:50:46 +0530

    working example done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
+import SearchInput,{ createFilter } from 'react-search-input'
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [users,setUsers] = useState(null)
+  const [filteredUsers,setFilter] = useState(null)
+  const KEYS_TO_FILTER = ['name.first','name.last']
+
+  useEffect(()=>{
+      const getRandomUsers = async()=>{
+
+            await axios.get('https://randomuser.me/api/')
+            .then(res =>{
+                console.log(res.data)
+                setUsers(res.data)
+            })
+            .catch(err=>{
+              console.log(err)
+            })
+
+      }
+      getRandomUsers();
+      console.log(users)
+  },[users !=null])
+
+   const searchUpdated = (term) =>{
+      const filtered = users.results.filter(createFilter(term,KEYS_TO_FILTER))
+      setFilter(filtered)
+      console.log(filtered)
+      console.log(filteredUsers)
+    }
+
+    return (
+      <div className="App">
+        <SearchInput className="search-input"  onChange={searchUpdated} />
+        {filteredUsers ? 
+
+          filteredUsers.map((data)=>(
+             <React.Fragment>
+                  <p> {data.name.first} {data.name.last} </p>
+              </React.Fragment>
+
+           )) : <p>Loading ...</p> 
+      
+      }
+        
+      </div>
+    );
+
+   
+}
 export default App;
